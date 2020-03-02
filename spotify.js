9fdedc18ab60d16115899757cfd589b34f2678a7
jcryer <jjcryer@mac.com>
2020-03-02 22:22:01 +0000

    Changed post data type

--- spotify.js
+++ spotify.js
@@ -2,13 +2,19 @@ const http = require('http');
 
 
 function getData(authCode) {
-    var data = JSON.stringify({
+    /*var data = JSON.stringify({
         grant_type: 'authorization_code',
         code: authCode,
         redirect_uri: "https://csed-server.herokuapp.com/callback",
         client_id: "295687b97d8f4da38afd639684a8a60e",
         client_secret: "e82a2d8d37d1436fa01d9ad332a1e00b"
-    });
+    });*/
+
+    var data = 'grant_type=authorization_code' +
+    '&code=' + authCode +
+    '&redirect_uri=https://csed-server.herokuapp.com/callback' +
+    '&client_id=' + '295687b97d8f4da38afd639684a8a60e' + 
+    '&client_secret=' + 'e82a2d8d37d1436fa01d9ad332a1e00b';
     
     var options = {
         hostname: 'accounts.spotify.com',
@@ -16,7 +22,7 @@ function getData(authCode) {
         path: '/api/token',
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': data.length
         }
     };
