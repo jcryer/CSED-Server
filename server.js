f4b04c72692740f1149da0ca6f00f9f0d3ee2738
jcryer <jjcryer@mac.com>
2020-03-02 16:47:41 +0000

    a

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const app = require('./app.js');
-var port = process.env.Port || 3000;
+var port = process.env.PORT || 3000;
 
 const options = {
     keys: fs.readFileSync('key.pem'),
