74bcbad9f9cbcdc8d79b2be2315337f3ba3cca42
jcryer <jjcryer@mac.com>
2020-03-05 18:24:46 +0000

    Login screen v1

---
+++ public/login.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('#submit').click(function() {
+		var username = $('#username').val();
+        var password = $('#password').val();
+        console.log(username);
+		//$.post("https://csed-server.herokuapp.com/api/login/", {"username": username, "password": password});
+        console.log(JSON.stringify({"username": username, "password": password}));
+		$.ajax({
+			url: "api/login",
+			type: "POST",
+			data: JSON.stringify({"username": username, "password": password}),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json",
+			success: function() {
+				console.log("Works");
+			}
+		});
+	});
+});
+
