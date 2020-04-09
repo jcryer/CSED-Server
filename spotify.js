77b31771d4f4cdb4728ec7861076e6b342c16f54
jcryer <jjcryer@mac.com>
2020-04-09 16:09:12 +0100

    -

--- spotify.js
+++ spotify.js
@@ -32,14 +32,14 @@ const getTracksInfo = async function(username, userid) {
 
   trackIDs = [];
   tracks = [];
-  for (let i = 0; i < listens.count; i++) {
+  for (var i = 0; i < listens.length; i++) {
     trackIDs.push(listens[i].songid);
     if (i % 48 == 0) {
       var tList = await spotifyApi.getTracks(trackIDs);
       tList.body.tracks.forEach(function(t) {
         tracks.push(t);
       });
-      console.log(tracks.count);
+      console.log(tracks.length);
       trackIDs = [];
     }
   }
