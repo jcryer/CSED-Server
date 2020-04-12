50587e464885d76cf39e4a2f51a10c05e0376ba4
jcryer <jjcryer@mac.com>
2020-04-12 21:35:21 +0100

    track null check

--- spotify.js
+++ spotify.js
@@ -90,7 +90,7 @@ async function sortUserSongs(username, userid) {
         }
         for (var j = 0; j < tracks.items.length; j++) {
           var item = tracks.items[j].track;
-          if (!(item.id in allTracks)) {
+          if (!(item.id in allTracks) && item.id != null) {
 
             allTracks[item.id] = item;
           }
