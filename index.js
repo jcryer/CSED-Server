fa09de4025614c9b109c918f3a325f487a1d29ab
jcryer <jjcryer@mac.com>
2020-04-14 13:50:25 +0100

    .

--- index.js
+++ index.js
@@ -161,12 +161,28 @@ router.get('/api/getTracks', verifyToken, async function(req, res) {
 
 const moods = Object.freeze({0: "Neutral", 1: "Happy", 2: "Sad", 3: "Calm", 4: "Energetic", 5: "Exuberance", 6: "Lively", 7: "Joyful", 8: "Contentment", 9: "Relaxing", 10: "Frantic", 11: "Depressing", 12: "Melancholic", 13: "For Concentration", 14: "Motivational" });
 
-router.get('/testing2', verifyToken, async function(req, res) {
+router.get('/api/recentMoods', verifyToken, async function(req, res) {
+  var data = await spotify.getTracksInfo(req.user.username, req.user.id);
+  var output = "test<br><br>";
+  for (var i = 0; i < data.length; i++) {
+
+    const diffTime = Math.abs(new Date(data[i].listen.played) - new Date());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    console.log(diffDays);
+    if (diffDays == 1) {
+      output += i+1 + ": <b>" + data[i].name + "</b>, by " + data[i].artist + "  | " + data[i].listen.played + " | <a href='" + data[i].uri + "'>Click here</a><br><br>";
+    }
+  }
+  res.send(output);
+});
+
+router.get('/moodClassification', verifyToken, async function(req, res) {
   var data = await spotify.sortUserSongs(req.user.username, req.user.id);
   var output = "";
   for (var mood in moods) {
-    output += "<a href='#" + mood + "'>" + moods[mood] + "</a><br>";
+    output += "<a href='#" + mood + "'>" + moods[mood] + "</a>   ";
   }
+  output += "<br><br>";
   for (var mood in moods) {
     output += "<h2 id='" + mood + "'>" + moods[mood] + "</h2>";
     var filtered = Object.keys(data).reduce(function (filtered, key) {
