45d0fec30d8326539ae1340ce78837242cc76bf1
jcryer <jjcryer@mac.com>
2020-04-14 11:10:09 +0100

    -

--- index.js
+++ index.js
@@ -161,18 +161,21 @@ router.get('/api/getTracks', verifyToken, async function(req, res) {
 
 const moods = Object.freeze({0: "Neutral", 1: "Happy", 2: "Sad", 3: "Calm", 4: "Energetic", 5: "Exuberance", 6: "Lively", 7: "Joyful", 8: "Contentment", 9: "Relaxing", 10: "Frantic", 11: "Depressing", 12: "Melancholic", 13: "For Concentration", 14: "Motivational" });
 
-
 router.get('/testing2', verifyToken, async function(req, res) {
   var data = await spotify.sortUserSongs(req.user.username, req.user.id);
   var output = "testing2<br><br>";
-  try {
-  for (var i in data) {
-    output += i + ": <b>" + data[i].name + "</b>, by " + data[i].artists[0].name + " - <b>" + moods[data[i].mood] + "</b><br><br>";
+
+  for (var mood in moods) {
+    output += "<h2>" + moods[mood] + "</h2>";
+    var filtered = Object.keys(data).reduce(function (filtered, key) {
+      if (dict[key].mood == mood) filtered[key] = dict[key];
+      return filtered;
+    }, {});
+    for (var i in filtered) {
+      output += i + ": <b>" + filtered[i].name + "</b>, by " + filtered[i].artists[0].name + " - <b>" + moods[filtered[i].mood] + "</b><br><br>";
+    }
+    output += "<br><br>";
   }
-}
-catch (e) {
-  console.error(e);
-}
   res.send(output);
 });
 
